ad1c7d8ae6c24ea9907dc97a697efd3261f5723f
Nazeeridreesi <Nazeeridreesi381@gmail.com>
2016-12-27 17:04:06 +0500

    assingments

---
+++ Assingment 9/Basic Calculator/js/myscript.js
@@ -0,0 +1,92 @@
+
+function addition(){
+    
+    var a = document.getElementById("numberone").value;
+    var b = document.getElementById("numbertwo").value;
+    
+    var num1 = parseInt(a);
+    var num2 = parseInt(b);
+    
+    var add = num1 + num2;
+    
+    document.getElementById("ans").innerHTML = add;
+}
+
+function substraction(){
+    
+    var a = document.getElementById("numberone").value;
+    var b = document.getElementById("numbertwo").value;
+    
+    var num1 = parseInt(a);
+    var num2 = parseInt(b);
+    
+    var add = num1 - num2;
+    
+    document.getElementById("ans").innerHTML = add;
+}
+
+function multiplication(){
+    
+    var a = document.getElementById("numberone").value;
+    var b = document.getElementById("numbertwo").value;
+    
+    var num1 = parseInt(a);
+    var num2 = parseInt(b);
+    
+    var add = num1 * num2;
+    
+    document.getElementById("ans").innerHTML = add;
+}
+
+function division(){
+    
+    var a = document.getElementById("numberone").value;
+    var b = document.getElementById("numbertwo").value;
+    
+    var num1 = parseInt(a);
+    var num2 = parseInt(b);
+    
+    var add = num1 / num2;
+    
+    document.getElementById("ans").innerHTML = add;
+}
+
+function ramainder(){
+    
+    var a = document.getElementById("numberone").value;
+    var b = document.getElementById("numbertwo").value;
+    
+    var num1 = parseInt(a);
+    var num2 = parseInt(b);
+    
+    var add = num1 % num2;
+    
+    document.getElementById("ans").innerHTML = add;
+}
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
